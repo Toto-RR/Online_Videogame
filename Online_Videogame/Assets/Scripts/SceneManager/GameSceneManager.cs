789899f192f156e11ff7d033ea8888af966b6fb5
Toni Romanos <antonio.romanos@estudiantat.upc.edu>
2024-11-20 02:35:48 +0100

    DONE: Player optimizated

--- Online_Videogame/Assets/Scripts/SceneManager/GameSceneManager.cs
+++ Online_Videogame/Assets/Scripts/SceneManager/GameSceneManager.cs
@@ -4,8 +4,7 @@ public class GameSceneManager : MonoBehaviour
 {
     public UDP_Server hostServerScript;      // Script para el host
     public UDP_Client clientScript;          // Script para el cliente
-    public GameObject hostPlayer;            // Prefab del jugador del Host
-    public GameObject clientPlayer;          // Prefab del jugador del Cliente
+    public GameObject playerPrefab;            // Player Prefab
     public GameConfigSO gameConfig;          // Referencia al GameConfigSO
 
     void Start()
@@ -24,17 +23,15 @@ public class GameSceneManager : MonoBehaviour
             // Si es Host
             hostServerScript.enabled = true;   // Activar el servidor (host)
             clientScript.enabled = false;      // Desactivar el script del cliente
-            hostPlayer.SetActive(true);        // Activar el jugador del host
-            clientPlayer.SetActive(false);     // Desactivar el jugador del cliente
         }
         else
         {
             // Si es Cliente
             clientScript.enabled = true;      // Activar el script del cliente
             hostServerScript.enabled = false; // Desactivar el script del host
-            clientPlayer.SetActive(true);     // Activar el jugador del cliente
-            hostPlayer.SetActive(false);      // Desactivar el jugador del host
         }
+
+        playerPrefab.SetActive(true);
     }
 
     bool DetermineIfHost()
